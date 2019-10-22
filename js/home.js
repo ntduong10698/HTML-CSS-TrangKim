6116d2e3ce01f07457297e229f13ba1f55096655
Do Xuan Hieu <anhhieuaxy@gmail.com>
2019-10-22 13:43:00 +0700

    code 1h42

--- js/home.js
+++ js/home.js
@@ -1,12 +1,9 @@
 $(document).ready(function () {
-console.log("hihi")
-
-   
     $(".block-menu-2 >ul >li> a").click(function () {
        $(this).siblings(".menu2-layer2").toggle(".d-inline");
+       $(this).children("img").toggleClass("rotate")
       })
     $(".menu2-layer2> ul> li >a").click(function () {
-      
         $(this).siblings(".menu2-layer-3").toggle(".d-inline");;
       })
 });
